a943de16bb9cbfdabd7ec77857dfd3609b9fed5d
Jeesun Bari <jeesunbari2002@gmail.com>
2026-08-11 18:32:21 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactCompiler: true,
   env: {
     TMDB_ACCESS_TOKEN:
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGIyOGRmMTVkY2YwMzIwYmNiMmFlYzZkZGI5OTlkMyIsIm5iZiI6MTcxMTcxMzA1OC4xMDYsInN1YiI6IjY2MDZhYjIyZTFmYWVkMDE2NGY5ZThhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EBcYVDC2v_uYAtUrMSSP4FkhfEoqpEHWmRXfQX-M3oo",
-    TMDB_API_KEY: "88b28df15dcf0320bcb2aec6ddb999d3",
+      "",
+    TMDB_API_KEY: "",
   },
   images: {
     remotePatterns: [
